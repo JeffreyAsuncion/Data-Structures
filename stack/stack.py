983ef600eaafb967a76a0661abc15d06a4eb1c86
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-15 07:38:36 -0500

    Stack Class implemented using an array as underlying storage

--- stack/stack.py
+++ stack/stack.py
@@ -17,29 +17,39 @@ https://www.geeksforgeeks.org/stack-data-structure-introduction-program/
 
 https://www.knowledgehut.com/blog/programming/how-to-implement-python-stack
 """
+
+"""
+Condition 1. 
+
+1. Implement the Stack class using an array as the underlying storage structure.
+   Make sure the Stack tests pass.
+"""
 from sys import maxsize
 
 class Stack:
     def __init__(self):
         self.size = 0
         self.storage = []
-        # self.storage = ?
+
 
     def __len__(self):
-        self.size = len(self.storage)
+        #self.size = len(self.storage)
         return self.size
 
 
     def push(self, value):
         self.storage.append(value)
+        self.size += 1
         #print(value + " pushed to stack")
 
     def pop(self):
-        # if self.size<=0:
-        #     return ("Stack Empty!")
-        # item = self.storage.pop()
-        # self.top -= 1
-        # return item
+        # if the storage is empty
         if not self.storage:
-            return str(-maxsize - 1) # return minus inifinite
+            #return None
+            return None
+        # otherwise 
+        # decrement size
+        # storage.pop
+        self.size -= 1
         return self.storage.pop()
+
