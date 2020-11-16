dc20707014be19eb9fcd00fa4cdc0735b5f9ca57
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-16 10:13:23 -0500

    singly_linked_lists_cspt12 notes

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -5,7 +5,105 @@ NOT inheritance of Node
 video CS02_Data_Structures_1
 """
 
+"""
+Elissa Thomas
+"""
 
+# class Node:
+#     def __init__(self, value=None, next_node=None):
+#         self.value = value
+#         self.next_node = next_node
+
+#     def get_value(self):    # getter
+#         return self.value
+    
+#     def get_next_node(self):
+#         return self.next_node
+    
+#     def set_next_node(self, new_next):
+#         self.next_node = new_next
+
+
+# class LinkedList:
+#     def __init__(self):
+#         # what attributes do we need?
+#         # start with an empty list
+#         self.head = None
+#         self.tail = None
+
+#     def add_to_head(self, value):
+#         # create a new Node
+#         new_node = Node(value)
+        
+#         # case 1 if LinkedList is empty
+#         if self.head is None:
+#             ## create a new Node    # see before if
+#             # update head and tail attributes
+#             self.head = new_node
+#             self.tail = new_node
+        
+#         # case 2 LinkedList has 1 item
+#         # code for case 2 is same as case 3   
+
+#         # case 3 for existing LinkedList
+#         else:
+#             ## create a new Node    # see before if
+#             # set next_node of my new Node to the head
+#             new_node.set_next_node(self.head)
+#             # update head attribute
+#             self.head = new_node
+
+
+#     def add_to_tail(self, value):
+#         # create a new Node
+#         new_node = Node(value)
+#         # 1. LL is empty
+#         if self.head is None:
+#             # update head and tail attributes
+#             self.head = new_node
+#             self.tail = new_node
+#         # 2. LL is not empty
+#         else:
+#             # update next_node of our tail
+#             self.tail.set_next_node(new_node) # old tail point to new_node / new tail
+#             # update self.tail
+#             self.tail = new_node 
+####### 1:20:30
+
+#     def remove_head(self):
+#         # case 1 - empty list
+#         if self.head is None:
+#             return None
+#         else:
+#             # case 2 - list with one item
+#             ret_value = self.head.get_value() # save value before remove head
+#             if self.head == self.tail:
+#                 self.head = None
+#                 self.tail = None
+#             # case 2 - list with 2+ elements - return VALUE of old head
+#             else:
+#                 self.head = self.head.get_next_node()
+#                 return ret_value
+
+
+#     def remove_tail(self):
+#         # TODO
+#         pass
+
+#     def contains(self, value):
+#         # TODO time permitting
+#         pass
+
+#     def get_max(self):
+#         # TODO time permitting
+#         pass
+
+
+
+
+"""
+CPT11 - Glenna Yu
+"""
 class Node:
     def __init__(self, value, next=None):
         self.value = value
